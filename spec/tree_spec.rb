e0a9ac439e8fe8e1e9dd34c732fddfef3626a471
Creeland <cree@provinsal.com>
2016-11-15 14:25:11 -0800

    Broken solution

--- spec/tree_spec.rb
+++ spec/tree_spec.rb
@@ -1,10 +1,39 @@
 require "spec_helper"
 require_relative "../tree.rb"
 
-Rspec.describe do
+RSpec.describe do
+  # Children
+  first_child = Tree.new(4, [])
+  second_child = Tree.new(5, [])
+  third_child = Tree.new(6, [])
+  fourth_child = Tree.new(7, [])
+
+  # Parents
+  first_parent = Tree.new(2, [first_child, second_child])
+  second_parent = Tree.new(3, [third_child, fourth_child])
+
+  # Grandparent
+  ancestor = Tree.new(1, [first_parent, second_parent])  
+
   describe "depth_first tree traversal" do 
-    it "should return a payload of 1" do 
-      expect(depth_first(ancestor)).to eq(1)  
+    it "returns a payload of 1" do 
+      expect(depth_first(ancestor, 1)).to eq(1)  
     end
+
+    it "returns the first parent's payload" do 
+      expect(depth_first(ancestor, 2)).to eq(2)
+    end
+
+    it "returns the first child's payload" do
+      expect(depth_first(ancestor, 4)).to eq(4)
+    end 
+
+    it "returns the second child's payload" do 
+      expect(depth_first(ancestor, 5)).to eq(5)
+    end
+
+    it "returns the third child's payload" do
+      expect(depth_first(ancestor, 6)).to eq(6)
+    end 
   end
 end
--- tree.rb
+++ tree.rb
@@ -1,3 +1,5 @@
+require 'byebug'
+
 class Tree
   attr_accessor :payload, :children
 
@@ -8,20 +10,29 @@ class Tree
 
 end
 
-# Grandparent
-ancestor = Tree.new(1, [first_parent, second_parent])
-
-# Parents
-first_parent = Tree.new(2, [first_child, second_child])
-second_parent Tree.new(3, [third_child, fourth_child])
-
 # Children
 first_child = Tree.new(4, [])
 second_child = Tree.new(5, [])
 third_child = Tree.new(6, [])
 fourth_child = Tree.new(7, [])
 
-def depth_first(node)
-  
-  depth_first(node)
+# Parents
+first_parent = Tree.new(2, [first_child, second_child])
+second_parent = Tree.new(3, [third_child, fourth_child])
+
+# Grandparent
+ancestor = Tree.new(1, [first_parent, second_parent])
+
+def depth_first(node, target, previous_nodes=[], child_count=0) 
+  return target if !node.nil? && node.payload == target 
+  previous_nodes.push(node) if node != previous_nodes.last
+
+  if node.children[child_count].nil?
+    previous_nodes.pop 
+    child_count += 1
+    return target if depth_first(previous_nodes.last, target, previous_nodes, child_count) == target
+  end
+
+  node = node.children[child_count]
+  depth_first(node, target, previous_nodes)
 end
